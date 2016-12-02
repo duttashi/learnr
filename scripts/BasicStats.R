0e40945c8d42b745b8e2f2900409253290c0a687
Ashish Dutt <ashish.dutt8@gmail.com>
2016-12-02 17:54:00 +0800

    added relevant comments

--- scripts/BasicStats.R
+++ scripts/BasicStats.R
@@ -8,6 +8,7 @@
 mt <- mtcars[c("mpg", "hp", "wt", "am")]
 head(mt)
 
+# DESCRIPTIVE STATISTICS FOR CONTINUOUS VARIABLES
 # Descriptive stats via summary
 summary(mt)
 # Descriptive stats via describe (Hmisc)
@@ -29,16 +30,18 @@ library(psych)
 myvars <- c("mpg", "hp", "wt")
 describeBy(mtcars[myvars], list(am=mtcars$am))
 
+# DESCRIPTIVE STATISTICS FOR CATEGORICAL VARIABLES
 # frequency tables
 library(vcd)
 head(Arthritis)
-# one way table
+
+# one way table can be generated using the table() function
 mytable <- with(Arthritis, table(Improved))
 mytable  # frequencies
 prop.table(mytable) # proportions
 prop.table(mytable)*100 # percentages
 
-# two way table
+# two way table where the xtabs() allows you to create a contigency table using formula style input
 mytable <- xtabs(~ Treatment+Improved, data=Arthritis)
 mytable # frequencies
 margin.table(mytable,1) #row sums
@@ -52,21 +55,29 @@ addmargins(mytable) # add row and column sums to table
 library(gmodels)
 CrossTable(Arthritis$Treatment, Arthritis$Improved)
 
+# Tests of independence
+# R provides several methods for testing the independence of categorical variables
 # Chi-square test of independence
 library(vcd)
 mytable <- xtabs(~Treatment+Improved, data=Arthritis)
-chisq.test(mytable)
+chisq.test(mytable) # there appears to be a relationship between treatment received and level of improvement (p<0.1)
+# because the propbability (p-value is small 0.01, we can reject the hypothesis that 
+#treatment type and outcome are independent)
+
 mytable <- xtabs(~Improved+Sex, data=Arthritis)
-chisq.test(mytable)
+chisq.test(mytable) # there does'nt appear to be a relationship between patient sex and improvement as p>0.5
+# because the propbability (p-value is greater than 0.05, 
+#we can accept the hypothesis that patient sex and improvement are independent)
 
 # Fisher's exact test
 mytable <- xtabs(~Treatment+Improved, data=Arthritis)
 fisher.test(mytable)
 
-
 # Chochran-Mantel-Haenszel test
 mytable <- xtabs(~Treatment+Improved+Sex, data=Arthritis)
 mantelhaen.test(mytable)
+# the results suggest that treatment received and improvement reported are not independent
+# within each level of sex 
 
 # Measures of association for a two-way table
 library(vcd)
@@ -74,10 +85,18 @@ mytable <- xtabs(~Treatment+Improved, data=Arthritis)
 assocstats(mytable)
 
 # Covariances and correlations
+# correlation coeffecients are used to describe relationships among quantitative variables.
+# The (+-) sign indicate the direction of the relationship (positive or inverse) and the
+# magnitude indicates the strength of the relationship (ranging from 0 for no relationship
+# to 1 for a perfect relationship)
+
+# Load the state.x77 dataset. Use help(state.x77) to learn more about the dataset
 states<- state.x77[,1:6]
 cov(states)
-cor(states)
-cor(states, method="spearman")
+cor(states) 
+cor(states, method="spearman") # we can see a strong positive correlation exist between
+# income and high school graduation rates and a strong negative correlation exist between
+# illiteracy rates and life expectancy
 
 x <- states[,c("Population", "Income", "Illiteracy", "HS Grad")]
 y <- states[,c("Life Exp", "Murder")]
@@ -91,6 +110,7 @@ pcor(c(1,5,2,3,6), cov(states))
 # Testing a correlation coefficient for significance
 cor.test(states[,3], states[,5])
 
+# Testing correlations for significance
 # Correlation matrix and tests of significance via corr.test
 library(psych)
 corr.test(states, use="complete")
