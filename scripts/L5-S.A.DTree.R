7a6a7b4b9bd559a9b9f21a2e8f7e87bccd732ef6
Ashish Dutt <ashish.dutt8@gmail.com>
2017-01-13 22:19:56 +0800

    initial commit

---
+++ scripts/L5-S.A.DTree.R
@@ -0,0 +1,8 @@
+# Decision Tree is a classification or supervised algorithm
+# The dataset use is the titanic data set from 
+str(Titanic)
+class(Titanic) # the bult in dataset is in table format
+# convert to a dataframe
+shipdata<-as.data.frame(Titanic)
+class(shipdata)
+str(shipdata)
