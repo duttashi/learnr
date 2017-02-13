9ca343edd20ccd12f0cc58d6df8014ba6179233a
Ashish Dutt <ashish.dutt8@gmail.com>
2017-02-13 19:26:55 +0800

    Add code: 1. Used missForest library for missing data imputation. Earlier tried the mice package but missing data persisted. 2. Hide df6 as its not required 3. add code for joining the tables using inner_join() of the dplyr library. 4. Univariate data visualization using histograms and density plots. The density plot showed variation or possible relations. 5. subset the data based on density plot visualizations 6. Boxplots for detecting outliers and excluded them by subsetting the data again.

--- scripts/CaseStudy-MY-LaborForce.R
+++ scripts/CaseStudy-MY-LaborForce.R
@@ -5,8 +5,9 @@ rm(list=ls())
 # Load the required libraries
 library(ggplot2)
 library(plyr)
+library(dplyr)
 library(tidyr) # for the gather()
-library(mice)
+library(missForest)
 
 # Load the data
 df1<- read.csv("data/bptms-Employed_by_state.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
@@ -14,7 +15,7 @@ df2<- read.csv("data/bptms-Labour_force_by_state.csv", header = TRUE, sep = ",",
 df3<- read.csv("data/bptms-Labour_Force_Participation_rate_by_state.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 df4<- read.csv("data/bptms-Outside_labour_force_by_state.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 df5<- read.csv("data/bptms-Unemployment_Rate.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-df6<- read.csv("data/bptms-Employed_less_than_30_hours.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+#df6<- read.csv("data/bptms-Employed_less_than_30_hours.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 # Exploratory Data Analysis
 dim(df1)
@@ -29,7 +30,8 @@ str(df2) # OBSERVATION: remove the comma between the numbers in employed attribu
 str(df3) # OBSERVATION: rename the col header to short names
 str(df4) # OBSERVATION: remove the comma between the numbers in employed attribute and change data type to numeric, rename the col header to short names
 str(df5) # OBSERVATION: rename the col header to short names
-str(df6) # OBSERVATION: rename the col header to short names
+table(df6$state) # state with 2 levels, one is blank and other is Malaysia. Drop df6. 
+rm(df6) # dropped df6
 
 sum(is.na(df1))
 sum(is.na(df2))
@@ -38,8 +40,6 @@ colSums(is.na(df3)) # check which col has missing values
 sum(is.na(df4))
 sum(is.na(df5)) # 29 missing values
 colSums(is.na(df5))
-sum(is.na(df6)) #2 missing values
-colSums(is.na(df6))
 
 # Basic Data Management 
 
@@ -59,51 +59,90 @@ df4<- rename(df4, c("Outside.Labour.Force...000." = "outlbrfrc"))
 names(df5)
 df5<- rename(df5, c("State.Country" = "state"))
 df5<- rename(df5, c("Unemployment.Rate..Percentage." = "unemprateperct"))
-names(df6)
-df6<- rename(df6, c("State.Country" = "state"))
-df6<- rename(df6, c("Working.less.than.30.hours...000." = "workless30"))
 
 ## Change data type
 df1$state<- as.factor(df1$state)
 df1$employed<- as.numeric(gsub(",","", df1$employed))
-
-str(df2)
 df2$state<- as.factor(df2$state)
 df2$labourforce<- as.numeric(gsub(",","", df2$labourforce))
-
-str(df3)
 df3$state<- as.factor(df3$state)
-
-str(df4)
 df4$state<- as.factor(df4$state)
 df4$outlbrfrc<- as.numeric(gsub(",","", df4$outlbrfrc))
-
-str(df5)
 df5$state<- as.factor(df5$state)
 
-str(df6)
-df6$state<- as.factor(df6$state)
+## Joining the data frames
+## using the dplyr library
+
+system.time(join1<- inner_join(df1,df2))
+system.time(join2<- inner_join(df3,df4))
+system.time(join3<- inner_join(join1,join2))
+system.time(df.master<- inner_join(join3,df5))
+
 
 # Missing value treatment
-colSums(is.na(df3))
-tempData <- mice(df3,m=5,maxit=50,meth='pmm',seed=1234)
-df3<-mice::complete(tempData,1)
-colSums(is.na(df3))
+## visualization
+library(VIM)
+aggr_plot <- aggr(df.master, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, 
+                  labels=names(df.master), cex.axis=.7, gap=3, 
+                  ylab=c("Histogram of missing data","Pattern"))
+colSums(is.na(df.master))
 
-colSums(is.na(df5))
-tempData <- mice(df5,m=5,maxit=50,meth='pmm',seed=1234)
-df5<-mice::complete(tempData,1)
-colSums(is.na(df5))
 
-colSums(is.na(df6))
-tempData <- mice(df6,m=5,maxit=50,meth='pmm',seed=1234)
-df6<-mice::complete(tempData,1)
-colSums(is.na(df6))
+imputdata<- missForest(df.master)
+# check imputed values
+imputdata$ximp
+df.cmplt<- imputdata$ximp
+colSums(is.na(df.cmplt))
 
-# Inner Join the data frames on common column
-df.m1<- merge(df1,df2, by="Year")
-df.m2<- merge(df3,df4, by="Year")
-df.m3<- merge(df.m2, df5, by="Year")
-#df.master<- merge(df.m1, df.m3, by="Year") # this code is problematic..  DO NOT EXECUTE. MEMORY ALLOCATION ERROR
+# Describe the data
+library(Hmisc)
+describe(df.cmplt)
 
+# Univariate Data Visualizations
+
+# Histogram's: NO GOOD IN SHOWING CONTINUOUS DATA
+ggplot(df.cmplt) +
+  geom_histogram(aes(x=Year), fill="gray", binwidth = 20, stat = "bin", position = "stack")
+
+ggplot(df.cmplt) +
+  geom_histogram(aes(x=employed), fill="gray", binwidth = 5000, stat = "bin", position = "stack")
+
+ggplot(df.cmplt) +
+  geom_histogram(aes(x=labourforce), fill="gray", binwidth = 5000, stat = "bin", position = "stack")
+ggplot(df.cmplt) +
+  geom_histogram(aes(x=lbrfrcperct), fill="gray", binwidth = 100, stat = "bin", position = "stack")
+
+# Density plots # FAR BETTER THAN HISTOGRAMS
+ggplot(df.cmplt)+
+  geom_density(aes(x=employed, fill="red"))
+ggplot(df.cmplt)+
+  geom_density(aes(x=labourforce, fill="red")) # majority of the labour force is >5000
+ggplot(df.cmplt)+
+  geom_density(aes(x=lbrfrcperct, fill="red")) # majority lies between 60-70%
+ggplot(df.cmplt)+
+  geom_density(aes(x=outlbrfrc, fill="red")) # majority of outside labour force is >2000
+ggplot(df.cmplt)+
+  geom_density(aes(x=unemprateperct, fill="red")) # majority lies between 2.5 and 5.0
+ggplot(df.cmplt)+
+  geom_density(aes(x=Year, fill="red")) # twin peaked, first peak at about 1988 and second peak at 2000-2005
+
+# subset the data based on observations from denisty plot
+subst.data.1<- subset(df.cmplt, labourforce<=1600 & lbrfrcperct <=70 & unemprateperct>=2.5
+                   & unemprateperct<=5.0) 
+# OBSERVATION: when labourforce <= 2000, there were outliers in the data. when labourforce<=1600 the outliers were removed
+
+
+# Boxplot
+
+library(magrittr) # for the pipe operator
+library(dplyr) # for select() 
+str(subst.data.1)
+
+boxplot(subst.data.1 %>% 
+          # Note that the ‘%>%’ (pipe) passes data from the command before to the one after.
+          select(employed,labourforce,outlbrfrc))
+boxplot(subst.data.1 %>%
+          select(unemprateperct))
+boxplot(subst.data.1 %>%
+          select(lbrfrcperct))
 
