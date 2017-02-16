4a7f4a4caaea257b8555f4eb6ee89cb7f1017ffa
Ashish Dutt <ashish.dutt8@gmail.com>
2017-02-16 17:57:08 +0800

    update code: 1. Commented changing data type of Year to factor. No need to change the data type for Year. 2. Added code for Basic statistics 3. Add code for outlier detection & treatment 4. Add code for correlation plots 5. Add code for multicollinearity detection 6. Add code for predictive analytics-(Work in progress)

--- scripts/CaseStudy-MY-LaborForce.R
+++ scripts/CaseStudy-MY-LaborForce.R
@@ -1,6 +1,7 @@
 # clear the workspace
 rm(list=ls())
-# Data Source Info
+
+# Data Source Info: Labor Statistics Data from 1982-2014
 # Labour Force and Social Statistic http://www.dosm.gov.my/v1/index.php?r=column3/accordion&menu_id=aHhRYUpWS3B4VXlYaVBOeUF0WFpWUT09
 # Load the required libraries
 library(ggplot2)
@@ -23,7 +24,7 @@ dim(df2)
 dim(df3)
 dim(df4)
 dim(df5)
-dim(df6)
+
 
 str(df1) # OBSERVATION: remove the comma between the numbers in Employed attribute and change data type to numeric, rename the col header to short names
 str(df2) # OBSERVATION: remove the comma between the numbers in Employed attribute and change data type to numeric, rename the col header to short names
@@ -72,12 +73,15 @@ df5$State<- as.factor(df5$State)
 
 ## Joining the data frames
 ## using the dplyr library
-
+library(dplyr)
 system.time(join1<- inner_join(df1,df2))
 system.time(join2<- inner_join(df3,df4))
 system.time(join3<- inner_join(join1,join2))
 system.time(df.master<- inner_join(join3,df5))
 
+str(df.master)
+# change Year to Factor
+#df.master$Year<- as.factor(df.master$Year)
 
 # Missing value treatment
 ## visualization
@@ -96,14 +100,40 @@ df.cmplt<- imputdata$ximp
 # check for missing values in the new data frame
 colSums(is.na(df.cmplt))
 
-# Describe the data
-library(Hmisc)
+# Describe the data or Basic Statistics
+library(Hmisc) # for the describe function
 describe(df.cmplt)
-summary(df.cmplt)
+
+# Frequency and Contigency tables for categorical data
+# One way table
 str(df.cmplt)
 
-# Univariate Data Visualizations
+mytable<- with(data=df.cmplt, table(State))
+mytable
+# contigency table
+mytable<- xtabs(~ State+LabrFrcPerct, data = df.cmplt)
+mytable
+### Proportion table
+prop.table(mytable)
 
+### Percentage table
+prop.table(mytable)*100
+
+# Test of independence for the categorical variable
+library(vcd) # for xtabs() and assocstats()
+mytable<- xtabs(~State+Employed, data= df.cmplt)
+chisq.test(mytable) # p value is greater than 0.05, indicating no relationship between state & employed
+                    # Note: The warning message is generated because one of the 17 cells (State- some Year) has an expected value less than five, which may invalidate the chi-square approximation.  
+mytable<- xtabs(~State+UnempRatePerct, data= df.cmplt)
+chisq.test(mytable) # # p value is less than 0.05, indicating a relationship between state & Unemployed rate percent
+summary(mytable)
+
+mytable<- xtabs(~State+LabrFrcPerct, data= df.cmplt)
+chisq.test(mytable) # # p value is less than 0.05, indicating a relationship between state & labour force percent
+# Summary: The significance test conducted using chi-square test of independence evaluates whether or not sufficient evidence exists to reject a null hypothesis of independence between the variables. We could not reject the null hypothesis for State vs Employed, Labour Force and Outside Labour Force. 
+# Unfortunately we cannot test the association between the two categorical variables State and Year for our dataset because the measures of association like Phi and Cramer's V require the categorical variables to have at least two levels example "Sex" got two levels, "Male", "Female". For reference purpose, use the assocstats() from the vcd package to test association
+
+# Univariate Data Visualizations
 # Histogram's: NO GOOD IN SHOWING CONTINUOUS DATA
 ggplot(df.cmplt) +
   geom_histogram(aes(x=Year), fill="gray", binwidth = 20, stat = "bin", position = "stack")
@@ -132,7 +162,14 @@ ggplot(df.cmplt)+
 
 # subset the data based on observations from denisty plot
 subst.data.1<- subset(df.cmplt, LabrFrc<=1600 & LabrFrcPerct <=70 & UnempRatePerct>=2.5
-                   & UnempRatePerct<=5.0) 
+                   & UnempRatePerct<=5.0)
+
+subst.data.2<- subset(df.cmplt, 
+                      (LabrFrcPerct>=60 & LabrFrcPerct <=70) & 
+                        (UnempRatePerct>=2.5 & UnempRatePerct<=5.0)
+                      )
+
+dim(subst.data.2)
 # OBSERVATION: when LabrFrc <= 2000, there were outliers in the data. when LabrFrc<=1600 the outliers were removed
 
 # Boxplot
@@ -148,6 +185,11 @@ boxplot(subst.data.1 %>%
 boxplot(subst.data.1 %>%
           select(LabrFrcPerct))
 
+boxplot(subst.data.2 %>% 
+          # Note that the ‘%>%’ (pipe) passes data from the command before to the one after.
+          select(Employed,LabrFrc,OutLabrFrc))
+
+
 #	Line plots- visualizing relationship between two variables
 ggplot(subst.data.1)+ geom_line(aes(x=Employed, y=LabrFrc, color="red"))
 ggplot(subst.data.1)+ geom_line(aes(x=LabrFrc, y=OutLabrFrc, color="red"))
@@ -155,5 +197,244 @@ ggplot(subst.data.1)+ geom_line(aes(x=Employed, y=OutLabrFrc, color="red"))
 ggplot(subst.data.1)+ geom_line(aes(x=LabrFrcPerct, y=UnempRatePerct, color="red"))
 
 # Barplot for categorical data visualization
-ggplot(subst.data.1)+ geom_bar(aes(x=Year, y=State), stat = "identity")
+counts<- table(df.cmplt$State)
+barplot(counts, main="State Distribution", xlab="Count of States")
 
+
+## OUTLIER DETECTION ##
+# Outliers treatment is a vital part of descriptive analytics since outliers can lead to misleading conclusions regarding our data.
+# For continuous variables, the values that lie outside the 1.5 * IQR limits
+# For categorical variables, outliers are considered to be the values of which frequency is less than 10%
+# outliers gets the extreme most observation from the mean. If you set the argument opposite=TRUE, it fetches from the other side.
+
+## VISUALLY DETECTING OUTLIER using BOXPLOTS
+# When reviewing a boxplot, an outlier is defined as a data point that is located outside the fences (“whiskers”) of the boxplot (e.g: outside 1.5 times the interquartile range above the upper quartile and bellow the lower quartile).
+# remember, ggplot2 requires both an x and y variable of a boxplot. Here is how to make a single boxplot
+
+p1<-ggplot(data= subst.data.2, aes(x="", y=Employed))+
+  geom_boxplot(outlier.size=2,outlier.colour="red")
+p2<-ggplot(data= subst.data.2, aes(x="", y=LabrFrc))+
+  geom_boxplot(outlier.size=2,outlier.colour="red")
+p3<-ggplot(data= subst.data.2, aes(x="", y=OutLabrFrc))+
+  geom_boxplot(outlier.size=2,outlier.colour="red")
+
+p1+ ggtitle("Employed in Malaysia (1982-2014)")+
+  xlab("")+ylab("Employed")
+p2+ ggtitle("Labour Force in Malaysia (1982-2014)")+
+  xlab("")+ylab("Labour Force")
+p3+ ggtitle("Outside Labour Force in Malaysia (1982-2014)")+
+  xlab("")+ylab("Outside Labour Force")
+
+# reset the par for graphics
+par(mfrow=c(1,1))
+
+# OUTLIER TREATMENT
+# subset the data 
+subst.data.3<- subset(df.cmplt, 
+                      (LabrFrc<=1600 & LabrFrcPerct>=60 & LabrFrcPerct <=70) & 
+                        (UnempRatePerct>=2.5 & UnempRatePerct<=5.0)
+                      )
+dim(subst.data.3)
+str(subst.data.3)
+
+p1<-ggplot(data= subst.data.3, aes(x="", y=Employed))+
+  geom_boxplot(outlier.size=2,outlier.colour="red")
+p2<-ggplot(data= subst.data.3, aes(x="", y=LabrFrc))+
+  geom_boxplot(outlier.size=2,outlier.colour="red")
+p3<-ggplot(data= subst.data.3, aes(x="", y=OutLabrFrc))+
+  geom_boxplot(outlier.size=2,outlier.colour="red")
+
+p1+ ggtitle("Employed in Malaysia (1982-2014)")+
+  xlab("")+ylab("Employed")
+p2+ ggtitle("Labour Force in Malaysia (1982-2014)")+
+  xlab("")+ylab("Labour Force")
+p3+ ggtitle("Outside Labour Force in Malaysia (1982-2014)")+
+  xlab("")+ylab("Outside Labour Force")
+
+## DATA VISUALIZATION for the Subset containing no outlier values to determine relationships
+str(subst.data.3)
+ 
+# Univariate Visualization: Plots you can use to understand each attribute standalone.
+
+## bar plot for categorical data
+ggplot(data = subst.data.3, aes(x=State))+
+  geom_bar(stat = "count", show.legend = TRUE)
+
+## Desnity and Boxplot for continuous data
+ggplot(subst.data.3)+
+  geom_density(aes(x=Employed, fill="red")) # two-peaked
+ggplot(subst.data.3)+
+  geom_density(aes(x=LabrFrc, fill="red")) # two-peaked
+ggplot(subst.data.3)+
+  geom_density(aes(x=LabrFrcPerct, fill="red")) # twin peaked 
+ggplot(subst.data.3)+
+  geom_density(aes(x=OutLabrFrc, fill="red")) # twin peaked 
+ggplot(subst.data.3)+
+  geom_density(aes(x=UnempRatePerct, fill="red")) # skewed with a righ tail
+
+par(mfrow=c(1,2))
+for(i in 1:2){
+  counts<- table(subst.data.3[,i])
+  name<- names(subst.data.3[,i])
+  barplot(counts, main = name)
+}
+
+par(mfrow=c(1,5),col.lab="blue", fg="indianred") # divide the screen into 1 row and five columns
+for(i in 3:7){
+  boxplot(subst.data.2[,i], main=names(subst.data.3[i]))
+}
+
+par(mfrow=c(1,5), col.lab="blue", fg="violetred4") # divide the graphics screen into 1 row and four columns
+par(mfrow=c(1,5)) # divide the screen into 1 row and four columns
+for(i in 3:7){
+  boxplot(subst.data.3[,i], main=names(subst.data.3[i]))
+}
+
+# For more colors in R, see this http://research.stowers-institute.org/efg/R/Color/Chart/index.htm
+for(i in 3:7){
+  plot(density(subst.data.3[,i]), main=names(subst.data.3[i]))
+}
+
+# NOTE: RESET GRAPHICAL PARAMETERS TO DEFAULT- In RStudio, You can just navigate to 'Plots' and select 'Remove plots' USE THE BROOM ICON
+
+## SCATTERPLOT MATRIX
+# pairwise scatterplot for continuous variables
+pairs(subst.data.3[,3:7])
+## scatterplot matrix by class
+str(subst.data.3)
+pairs(State~., data = subst.data.3, col=subst.data.3$State)
+
+# Generate scatterplots with Pearson correlations- BETTER PLOT THAN pairs()
+library(psych) #Calls: pairs.panels
+pairs.panels(subst.data.3)
+
+## Convert Year to 
+temp<- subst.data.3
+temp$Year<- as.numeric(temp$Year)
+
+## CORRELATION PLOTS
+
+# We can calculate the correlation between each pair of numeric attributes. These pair-wise correlations can be plotted in a correlation matrix plot to given an idea of which attributes change together.
+#LOAD LIBRARY
+library(corrplot)
+# calculate the correlations
+correlations<- cor(subst.data.3[,3:7])
+# plot the correlations
+corrplot(correlations, method = "number")
+# High Correlations: Employed - LaborForce; Employed - OutsideLaborForce; LaborForce - OutsideLaborForce;
+# OutsideLaborForce - Employed
+# correlation measures the relationship between two variables. When these two variables are so highly correlated that they explain each other (to the point that you can predict the one variable with the other), then we have collinearity (or multicollinearity).
+# One way to measure multicollinearity is the variance inflation factor (VIF), which assesses how much the variance of an estimated regression coefficient increases if your predictors are correlated.  If no factors are correlated, the VIFs will all be 1.
+
+## VIF plot for checking multicollinearity
+# A simple approach to identify collinearity among explanatory variables is the use of variance inflation factors (VIF).
+library(car) # for the vif () 
+mod<- lm(UnempRatePerct~., data=subst.data.3[,3:7])
+# Use function `vif` to calculate variance inflation factors for each variable in the model
+vif(mod)
+
+## MULTICOLLINEARITY TREATMENT
+## Method 1: Principal Component Analysis (PCA) reduces the number of predictors to a smaller set of uncorrelated components.
+# PCA can be applied only on numerical data. We’ll convert these categorical variables into numeric using one hot encoding.
+
+library(dummies)
+## find out how to dummy code the factors to numeric
+## reference: 
+df.convrtd<- subst.data.3
+
+
+for(level in unique(subst.data.3$State)){
+  df.convrtd[paste("dummy", level, sep = "_")] <- ifelse(subst.data.3$State == level, 1, 0)
+}
+head(df.convrtd)
+df.convrtd$Year<- as.numeric(df.convrtd$Year)
+str(df.convrtd)
+# drop variable State as its been dummy coded now
+df.convrtd$State<-NULL
+
+######## Splitting the dataset into train and test#####
+ratio = sample(1:nrow(df.convrtd), size = 0.25*nrow(df.convrtd))
+pca.test = df.convrtd[ratio,] #Test dataset 25% of total
+pca.train = df.convrtd[-ratio,] #Train dataset 75% of total
+
+#principal component analysis
+
+prin_comp <- prcomp(pca.train, scale. = T)
+names(prin_comp)
+biplot(prin_comp, scale = 0)
+#compute standard deviation of each principal component
+std_dev <- prin_comp$sdev
+#compute variance
+pr_var <- std_dev^2
+#check variance of first 10 components
+pr_var[1:10]
+#proportion of variance explained
+prop_varex <- pr_var/sum(pr_var)
+prop_varex[1:22]
+
+plot(prop_varex, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     type = "b") #15 components show max variance
+
+plot(cumsum(prop_varex), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     type = "b")
+
+train.data <- df.convrtd[,1:15]
+library(rpart)
+rpart.model <- rpart(UnempRatePerct ~ .,data = train.data, method = "anova")
+rpart.model
+
+#transform test into PCA
+test.data <- predict(prin_comp, newdata = pca.test)
+test.data <- as.data.frame(test.data)
+#select the first 15 components
+test.data <- test.data[,1:15]
+#make prediction on test data
+rpart.prediction <- predict(rpart.model, test.data) # ERROR, object Year not found
+
+### Feature Importance ###
+### work done on 15/Feb/2017 ###
+
+library(Boruta)
+# run boruta analysis
+set.seed(1234)
+# pull out the response variable
+response <- subst.data.3$UnempRatePerct
+bor.results <- Boruta(subst.data.3,response,
+                      maxRuns=101,
+                      doTrace=0)
+bor.results # basically, all the variables are considered important for response variable prediction
+cat("\n\nRelevant Attributes:\n")
+getSelectedAttributes(bor.results) # "Year"           "State"          "Employed"       "LabrFrc"        "OutLabrFrc"     "UnempRatePerct"
+plot(bor.results)
+
+######## Splitting the dataset into train and test#####
+ratio = sample(1:nrow(subst.data.3), size = 0.25*nrow(subst.data.3))
+Test = subst.data.3[ratio,] #Test dataset 25% of total
+Training = subst.data.3[-ratio,] #Train dataset 75% of total
+# Evaluation metric function
+RMSE <- function(x,y){
+  a <- sqrt(sum((log(x)-log(y))^2)/length(y))
+  return(a)
+}
+
+# Multiple Linear regression
+linear.mod<- lm(UnempRatePerct~., data = Training)
+summary(linear.mod)
+plot(linear.mod, pch=16, which = 1)
+predict<- predict(linear.mod, Test)
+
+RMSE0<- RMSE(predict, Test$UnempRatePerct)
+RMSE0
+
+# Calculate prediction accuracy and error rates
+actuals_preds <- data.frame(cbind(actuals=Test$UnempRatePerct, predicteds=predict)) # make actuals_predicteds dataframe.
+correlation_accuracy <- cor(actuals_preds)
+correlation_accuracy # 73% prediction accuracy
+
+min_max_accuracy <- mean (apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))
+min_max_accuracy # 89%
+
+mape <- mean(abs((actuals_preds$predicteds - actuals_preds$actuals))/actuals_preds$actuals)
+mape # 11%
