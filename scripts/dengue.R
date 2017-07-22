6541ab78ef012caf99904892b0a36e52573d3e1e
Ashish Dutt <ashish.dutt8@gmail.com>
2017-07-22 17:00:11 +0800

    added relevant comments

--- scripts/dengue.R
+++ scripts/dengue.R
@@ -2,12 +2,13 @@
 rm(list = ls())
 # read the data
 dengue.train<- read.csv("data/dengue_train.csv", header = TRUE, sep = ",")
+str(dengue.train$week_start_date)
 # split date 
 library(tidyr) # for separate()
 # separate the week_start_date into year, month and date format
 dengue.train <- separate(dengue.train, week_start_date, into= c("Year", "Month", "Day"), 
                       sep="-",extra = "drop")
-
+# convert character to numeric format
 dengue.train$Year<- as.numeric(dengue.train$Year)
 dengue.train$Month<- as.numeric(dengue.train$Month)
 dengue.train$Day<- as.numeric(dengue.train$Day)
