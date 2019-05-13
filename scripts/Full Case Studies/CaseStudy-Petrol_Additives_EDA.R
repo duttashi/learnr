813c95dc0cd15625063bec3dac9bcfa4093ab8d7
Ashish Dutt <ashish.dutt8@gmail.com>
2019-05-13 08:30:36 +0800

    modified script: no major changes

--- scripts/Full Case Studies/CaseStudy-Petrol_Additives_EDA.R
+++ scripts/Full Case Studies/CaseStudy-Petrol_Additives_EDA.R
@@ -8,7 +8,7 @@
 rm(list = ls())
 
 # required libraries
-library(caret) # for nearZeroVar()
+library(caret) # for nearZeroVar(), findCorrelation()
 library(cluster) # for gower() and pam()
 library(corrplot) # for correlation matrix
 library(dplyr) # for mutate()
@@ -110,6 +110,8 @@ plt+ geom_density(aes(y = ..count..), fill = "lightgray") +
 plt+geom_histogram(bins = 30, color = "black", fill = "gray") +
   geom_vline(aes(xintercept = mean(b)), 
              linetype = "dashed", size = 0.6)
+# clear the graphic device
+grid.newpage()
 
 #### PART C - Unsupervised Feature Selection
 # Conduct PCA for continuous variables
@@ -133,9 +135,9 @@ pc2<-fviz_contrib(petrol.data.new.pca, choice = "var", axes = 2, top = 10,
                   fill = "#b4a8d1")+
   labs(title="(C) additive feature")
 
-fig1<- grid.arrange(arrangeGrob(screeplot), 
+fig3<- grid.arrange(arrangeGrob(screeplot), 
                     arrangeGrob(pc1,pc2, ncol=1), ncol=2, widths=c(2,1)) 
-annotate_figure(fig1
+annotate_figure(fig3
                 ,top = text_grob("Clustering tendency of petrol additives", color = "black", face = "bold", size = 14)
                 ,bottom = text_grob("Some survey data", color = "brown",
                                     hjust = 1, x = 1, face = "italic", size = 10)
@@ -149,6 +151,7 @@ names(petrol.data.new)
 str(petrol.data.new)
 petrol.data.impvars<-as.data.frame(
   petrol.data.new[,c(1:3,5:6)])
+names(petrol.data.impvars)
 # clear the graphic device
 grid.newpage()
 
@@ -167,7 +170,7 @@ petrol.data.impvars[
   which(gower_mat == min(gower_mat[gower_mat != max(gower_mat)]),
         arr.ind = TRUE)[1, ], ]
 ###### Determine the clusters basis of distance calculated above
-pam_fit <- pam(gower_dist, diss = TRUE, k = 2)
+pam_fit <- pam(gower_dist, diss = TRUE, k = 3)
 pam_results <- petrol.data.impvars %>%
   mutate(cluster = pam_fit$clustering) %>%
   group_by(cluster) %>%
@@ -178,7 +181,7 @@ print(pam_results$the_summary)
 sink()  #  # returns output to the console
 pam_results$the_summary
 
-pam.res<-eclust(petrol.data.impvars, FUNcluster = c("pam"),k=2,graph = FALSE)
+pam.res<-eclust(petrol.data.impvars, FUNcluster = c("pam"),k=3,graph = FALSE)
 # Silhouette coeffecient Note: A score nearing to 1 is good
 p<-fviz_silhouette(pam.res, palette = "jco", ggtheme = theme_classic()) # 0.59
 annotate_figure(p,bottom = text_grob("Data source: \n Some survey data\n", color = "brown",
@@ -189,4 +192,4 @@ grid.rect(width = 1.00, height = 0.99,
           gp = gpar(lwd = 2, col = "black", fill=NA))
 grid.newpage()
 
-####### End of script
+###### END OF SCRIPT
